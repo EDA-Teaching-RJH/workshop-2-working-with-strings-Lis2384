8e94d16e0d13c47c3283551d38391e6dc015318f
Lis <lp598@kent.ac.uk>
2026-01-19 13:27:36 +0000

    working code

--- partOne.py
+++ partOne.py
@@ -1,8 +1,10 @@
 def main():
-    slow = input("Input ")
+    slow = input("Input:")
     myFunction(slow)
+    print(myFunction(slow))
 
 def myFunction(text):
   #Your code goes here.
-
+ T2 = text.replace(' ','...')
+ return(T2)
 main()
