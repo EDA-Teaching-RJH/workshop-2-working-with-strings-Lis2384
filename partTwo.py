12caced6ab39c8ac44816aa9a4cb28fecc512ec0
Lis <lp598@kent.ac.uk>
2026-01-19 13:51:01 +0000

    pythagoras

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,14 @@
 import math  
 
 def main():
-#TO DO  
+    maths1 = int(input("What is a =")) #TO DO  
+    maths2 = int(input("what is b ="))
+    print(pythag(maths1,maths2))
 
 def pythag(A,B):
-#TO DO  
+    mathsM = (A**2 + B**2)
+    mathsC = (mathsM**0.5)
+    return(mathsC)
+
 
 main()
